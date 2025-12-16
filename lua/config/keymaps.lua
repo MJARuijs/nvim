3f48120a43d6f1c8203dce09d1e6f2e7d2cb19bb
MJARuijs <mja.ruijs@gmail.com>
2025-12-16 12:16:56 +0100

    Update plugins and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- vim.keymap.del("n", "<A-h>")
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-q>", "<cmd><leader>bd<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-h>", "<cmd>vertical resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-l>", "<cmd>vertical resize +2<CR>", { noremap = true, silent = true })
