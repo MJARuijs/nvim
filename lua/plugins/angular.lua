5e3b86523d56e27cdcc3cbcb8ea7f89aeb7db483
MJARuijs <mja.ruijs@gmail.com>
2025-12-29 10:39:34 +0100

    Angular working???

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -1,9 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
   enabled = true,
-  config = function()
-    -- local lspconfig = require("lspconfig")
-    -- lspconfig.ts_ls.setup({})
-    -- lspconfig.angularls.setup({})
-  end,
+  -- config = function()
+  -- local lspconfig = require("lspconfig")
+  -- lspconfig.ts_ls.setup({})
+  -- lspconfig.angularls.setup({})
+  -- end,
 }
