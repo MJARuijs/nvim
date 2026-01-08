dad63c15dfbee75be874ca9a12387ed41c742cda
MJARuijs <mja.ruijs@gmail.com>
2026-01-08 17:06:42 +0100

    Probably broke the config.. Backing up just in case

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,7 +1,25 @@
 return {
-    -- "nvim-java/nvim-java",
-    -- config = function()
-    --     require("java").setup()
-    --     vim.lsp.enable("jdtls")
-    -- end,
+    "nvim-java/nvim-java",
+    config = function()
+        require("java").setup()
+        vim.lsp.enable("jdtls")
+        vim.keymap.set("n", "<leader>Jt", "<cmd>JavaTestRunCurrentClass<CR>", { desc = "Run Current Test File" })
+        vim.keymap.set("n", "<leader>Jr", "<cmd>JavaTestViewLastReport<CR>", { desc = "View Test Result" })
+        vim.lsp.config("jdtls", {
+            settings = {
+                java = {
+                    configuration = {
+                        runtimes = {
+                            {
+                                name = "JavaSE-21",
+                                -- path = "/opt/jdk-21",
+                                path = vim.fn.expand("/usr/lib/jvm/java-21-openjdk/"),
+                                default = true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+    end,
 }
