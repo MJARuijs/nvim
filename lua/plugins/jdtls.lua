c96218e2960eac770718ce7b1064672bbc3a582a
MJARuijs <mja.ruijs@gmail.com>
2026-01-10 19:23:59 +0100

    Added Java LSP.. Still work to do tho

---
+++ lua/plugins/jdtls.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function(args)
+    require("jdtls/jdtls_setup").setup()
+  end,
+})
+
+return { "mfussenegger/nvim-jdtls" }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,52 +1,40 @@
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-        -- "ts_ls",
-        -- "html",
-        -- "cssls",
-        -- "tailwindcss",
-        -- "svelte",
-        -- "lua_ls",
-        -- "graphql",
-        -- "emmet_ls",
-        -- "prismals",
-        -- "pyright",
-        -- "eslint",
-      },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
+      automatic_enable = {
+        exclude = {
+          "jdtls",
         },
       },
-      "neovim/nvim-lspconfig",
     },
   },
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "mason-org/mason.nvim",
     opts = {
-      ensure_installed = {
-        -- "prettier", -- prettier formatter
-        -- "stylua", -- lua formatter
-        -- "isort", -- python formatter
-        -- "black", -- python formatter
-        -- "pylint",
-        -- "eslint_d",
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
       },
     },
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
   },
+  -- {
+  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       -- "prettier", -- prettier formatter
+  --       -- "stylua", -- lua formatter
+  --       -- "isort", -- python formatter
+  --       -- "black", -- python formatter
+  --       -- "pylint",
+  --       -- "eslint_d",
+  --     },
+  --   },
+  --   dependencies = {
+  --     "williamboman/mason.nvim",
+  --   },
+  -- },
 }
