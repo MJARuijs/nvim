175d0b062eee3cbe536413cd90b2af91873af84c
MJARuijs <mja.ruijs@gmail.com>
2026-01-25 11:56:29 +0100

    Implement auto-colorscheme changing

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,15 +16,21 @@ autocmd("VimLeavePre", {
   command = ":silent !alacritty msg config 'window.padding={x=20,y=20}'",
 })
 
+-- i = 1
 autocmd("Signal", {
   pattern = "SIGUSR1",
   -- group = vim.api.nvim_create_augroup("toggle_bg_on_SIGUSR1", {}),
   callback = function()
     package.loaded.theme = nil
-    require("intellij")
-    vim.cmd([[colorscheme intellij]])
+
+    -- if math.fmod(i, 2) == 0 then
+    --   vim.cmd([[colorscheme intellij]])
+    -- else
+    --   vim.cmd([[colorscheme blue]])
+    -- end
+    -- i = i + 1
     vim.notify("Refreshed theme")
-    -- vim.cmd.colorscheme("blue")
+    vim.cmd.colorscheme("intellij")
     vim.schedule(function()
       -- without this, nvim window need to be focused for the effect take into account
       vim.cmd("redraw!")
