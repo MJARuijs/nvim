24e10792a3da7e4e4fb7ea721194c00f119a7c22
MJARuijs <mja.ruijs@gmail.com>
2026-03-09 08:57:12 +0100

    ESlint: disable warnings for empty functions

--- lua/plugins/lang/eslint.lua
+++ lua/plugins/lang/eslint.lua
@@ -2,10 +2,10 @@ return {
   "esmuellert/nvim-eslint",
   config = function()
     require("nvim-eslint").setup({
-
       settings = {
         rulesCustomizations = {
           { rule = "@angular-eslint/prefer-standalone", severity = "off" },
+          { rule = "@typescript-eslint/no-empty-function", severity = "off" },
         },
       },
     })
