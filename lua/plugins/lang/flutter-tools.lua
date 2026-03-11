fe755de86dc258424de7b95eb8443c30338a0f59
MJARuijs <mja.ruijs@gmail.com>
2026-03-11 09:19:59 +0100

    Add keymaps for Flutter

--- lua/plugins/lang/flutter-tools.lua
+++ lua/plugins/lang/flutter-tools.lua
@@ -5,5 +5,37 @@ return {
         "nvim-lua/plenary.nvim",
         "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
-    config = true,
+    config = function()
+        vim.keymap.set("n", "<C-f>", "", { desc = "Flutter" })
+        vim.keymap.set("n", "<leader>F", "", { desc = "Flutter" })
+
+        vim.keymap.set("n", "<leader>Fo", function()
+            vim.cmd("FlutterOutlineToggle")
+        end, { desc = "Toggle Outline" })
+
+        vim.keymap.set("n", "<leader>Fa", function()
+            vim.cmd("FlutterAttach")
+        end, { desc = "Attach" })
+
+        vim.keymap.set("n", "<leader>Fd", function()
+            vim.cmd("FlutterDevices")
+        end, { desc = "Devices" })
+
+        vim.keymap.set("n", "<leader>Ft", function()
+            vim.cmd("FlutterLogToggle")
+        end, { desc = "Toggle Terminal" })
+
+        vim.keymap.set("n", "<leader>Fc", function()
+            vim.cmd("FlutterLogClear")
+        end, { desc = "Clear Terminal" })
+
+        vim.keymap.set("n", "<leader>Fr", function()
+            vim.cmd("FlutterRun")
+        end, { desc = "Run" })
+
+        vim.keymap.set("n", "<leader>Fq", function()
+            vim.cmd("FlutterQuit")
+        end, { desc = "Quit" })
+    end,
+    -- config = true,
 }
