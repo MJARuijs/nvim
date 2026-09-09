60876a20f5c4c18df299512ad724fe3c2d81e764
MJARuijs <mja.ruijs@gmail.com>
2026-09-09 15:16:43 +0200

    backup

--- lua/plugins/tabset.lua
+++ lua/plugins/tabset.lua
@@ -14,8 +14,7 @@ return {
                 --     expandtab = true,
                 -- },
                 {
-                    filetypes = { "lua", "java", "kotlin" },
-                    -- filetypes = { "javascript", "typescript", "typescriptreact", "jsx", "css", "scss", "xml", "yml" },
+                    filetypes = { "javascript", "typescript", "typescriptreact", "jsx", "css", "scss", "xml", "yml" },
                     config = {
                         tabwidth = 2,
                         shiftwidth = 2,
