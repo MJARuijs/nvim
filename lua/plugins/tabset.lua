d5817f92ca4c3e19c23972d845aed988d79c0a5e
MJARuijs <mja.ruijs@gmail.com>
2026-09-11 09:50:55 +0200

    Add HTML and HTMLAngular to TabSet config

--- lua/plugins/tabset.lua
+++ lua/plugins/tabset.lua
@@ -14,7 +14,7 @@ return {
                 --     expandtab = true,
                 -- },
                 {
-                    filetypes = { "javascript", "typescript", "typescriptreact", "jsx", "css", "scss", "xml", "yml" },
+                    filetypes = { "javascript", "html", "htmlangular", "typescript", "typescriptreact", "jsx", "css", "scss", "xml", "yml" },
                     config = {
                         tabwidth = 2,
                         shiftwidth = 2,
